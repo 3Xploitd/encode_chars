8733dbba9a50c045763bcaf6311d7caae222225d
3Xploitd <40121116+3Xploitd@users.noreply.github.com>
2023-03-22 12:36:18 -0500

    Create encode_text.py

---
+++ encode_text.py
@@ -0,0 +1,11 @@
+import sys
+
+def encode_text(string):
+    text = []
+    for line in string:
+        line = hex(ord(line))
+        text.append(f'chr({line})')
+    print("±".join(text))
+    
+    
+encode_text(sys.argv[1])
